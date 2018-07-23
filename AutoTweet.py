fd9fe15c2c7f1089f8112e1f341b112f2d356a3f
Senthil Kumar <senthil1997@gmail.com>
2018-07-23 09:10:28 +0530

    auto retweeter

---
+++ AutoTweet.py
@@ -0,0 +1,38 @@
+import tweepy
+from textblob import TextBlob 
+
+consumer_key = ''
+consumer_secret = ''
+
+labels = []
+access_token = ''
+access_token_secret = ''
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api=tweepy.API(auth)
+
+
+def rtweet():
+    public_tweets = api.search('string')
+
+        #needs error handling (cant retweet if already rted)
+    for tweet in public_tweets:
+        print (tweet.id)
+        text=TextBlob(tweet.text)
+        print(text)
+        api.retweet(tweet.id)
+
+
+def deleteall():
+    tweets=api.user_timeline() #gathers all tweets from authenticated uesr
+    for tweet in tweets:
+        api.destroy_status(tweet.id)
+
+
+#rtweet()
+#deleteall()
+
+
+
