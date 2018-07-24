10d4d175455ecaafd9a789cb5c27b13c083f95be
Sagar Vakkala <sagarwakawaka@gmail.com>
2018-07-24 22:01:51 +0530

    Added comments

--- FTP Stuff/ftblib_BasicFTP_Server.py
+++ FTP Stuff/ftblib_BasicFTP_Server.py
@@ -7,18 +7,24 @@ Written by Sagar Vakkala @ionicc
         Even if used personally, Make sure to change the login username and id.
 
 """
+# Import the FTB module from the ftplib package
 
 from ftplib import FTP
 
-ip = FTP('000.000.0.0')
+#Make a FTP server instance on your local ip
 
-ip.login(user='admin', passwd='pass')
+ip = FTP('000.000.0.0') #Replace this with your local ip
 
+#Credentials to login to the server
+ip.login(user='admin', passwd='pass') #This is completely opetion, But still if you use it. Change the username and password to something not that obvious
+
+#The path used
 ip.cwd('Enter path')
 
 
 def getFile():
     file_name = 'something.txt'
+    # wb = Write binary, rb = Read Binary
     local_file = open(file_name, 'wb')
     ip.retrbinary(file_name, local_file.write, 1024)
     ip.quit()
