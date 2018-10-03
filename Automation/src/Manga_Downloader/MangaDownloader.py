48c75a6229e10412a721cf8ec06963eaa06579e1
Senthil Kumar <senthil1997@gmail.com>
2018-10-03 13:32:59 +0530

    Updated MangaDownloader
    Upgraded to headless Chrome.
    Allows for command line interface and automatically creates new folder for downloading manga.

--- Automation/src/Manga_Downloader/MangaDownloader.py
+++ Automation/src/Manga_Downloader/MangaDownloader.py
@@ -6,14 +6,21 @@
 import requests
 from bs4 import BeautifulSoup
 import os
+import sys
 import selenium
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options  
+
 
 chapter=1
 page=1
 MangaName=''
-website='https://www.mangapanda.com' 
+website='https://www.mangapanda.com'
+#MangaName=input('Enter the manga that you want to download : ')
+
+
+    
 
 def downloadImg(url):
     r=requests.get(url)
@@ -28,33 +35,46 @@ def downloadImg(url):
         with open("%s.jpg" %filename,'wb')as f:
                 for chunk in response.iter_content(4096):
                     f.write(chunk)
-
- 
-driver = webdriver.Chrome(executable_path=r"chromedriver.exe") #need to change to phantom.js
-driver.get("https://www.mangapanda.com/search")
-searchBar = driver.find_element_by_xpath('//*[@id="searchinput"]')
-searchBar.send_keys(MangaName)
-searchBar.send_keys(Keys.RETURN)
-print(driver.current_url)
-
-resp=requests.get(driver.current_url)
-d=resp.text
-
-s=BeautifulSoup(d,"lxml")
-lis=s.find_all('a',href=True)
-manga=lis[7]['href']
+while True:
+    try:
+        chapter=1
+        page=1
+        MangaName=input('Enter the manga that you want to download : ')
+        os.mkdir(MangaName)
 
 
+        chrome_options = Options()  
+        chrome_options.add_argument("--headless")  
+        driver = webdriver.Chrome(  chrome_options=chrome_options)
+        driver.get("https://www.mangapanda.com/search")
+        searchBar = driver.find_element_by_xpath('//*[@id="searchinput"]')
+        searchBar.send_keys(MangaName)
+        searchBar.send_keys(Keys.RETURN)
+        print(driver.current_url)
+    
+        resp=requests.get(driver.current_url)
+        d=resp.text
+
+        s=BeautifulSoup(d,"lxml")
+        lis=s.find_all('a',href=True)
+        manga=lis[7]['href']
 
 
-url=website+manga+'/'+str(chapter)+ '/'+str(page)
 
-check=requests.get(url)
-print (check)
 
-while check:
-    downloadImg(url)
-    page=page+1
-    url=website+manga+'/'+str(chapter)+ '/'+str(page)
-    print(url)
-    check=requests.get(url)
+        url=website+manga+'/'+str(chapter)+ '/'+str(page)
+
+        check=requests.get(url)
+        #print (check)
+        cwd=os.getcwd()
+        cwd=cwd+'/'+MangaName
+        os.chdir(cwd)
+        print("Downloading...")
+        while check:
+            downloadImg(url)
+            page=page+1
+            url=website+manga+'/'+str(chapter)+ '/'+str(page)
+            print(manga+'/'+str(chapter)+'/'+str(page))
+            check=requests.get(url)
+    except FileExistsError:
+        print("Manga already exists")
