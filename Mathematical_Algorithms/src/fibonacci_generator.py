7abc08a90ab6b0950361d6108840c04117b29712
nizarion <nizaralmesry@hotmail.com>
2018-10-14 18:09:48 +0100

    Added fib genarator

---
+++ Mathematical_Algorithms/src/fibonacci_generator.py
@@ -0,0 +1,10 @@
+"""Print Fibonacci using Generators"""
+a = int(input('Give range: '))
+
+def fib(n):
+    a, b = 0, 1
+    for _ in range(n):
+        yield a
+        a, b = b, a + b
+
+print(list(fib(a)))
