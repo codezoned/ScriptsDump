0c5321040fa9a5ddae3eeb4c814f73727a84f60c
Nishant Nahata <nishant.nahata97@gmail.com>
2018-10-16 11:51:32 +0530

    Corrected and improved binary_search

--- Arrays-searching/src/binary_search/binary_search.py
+++ Arrays-searching/src/binary_search/binary_search.py
@@ -1,22 +1,36 @@
 """
-   Written By @searpheon - Arka
-   BINARY SEARCH
+	Written By @searpheon - Arka
+	Updated By @nishantcoder97 - Nishant Nahata
+	BINARY SEARCH
 """
 
-def binary_search(item_list,item): //creating the function
-	first = 0 //first element
-	last = len(item_list)-1 //self explanatory
-	found = False //setting value top false if we find the element we are looking for
-	while( first<=last and not found):
-		mid = (first + last)//setting value of middle element
-		if item_list[mid] == item :
-			found = True
+"""
+	Condition: List should be sorted in ascending order
+"""
+def binary_search(item_list, item):
+	"""
+	param: list item_list: List to be searched
+	param: int item: Item to be searched for
+	returns: int index: Index of the first occurrence of item, or len(tem_list) if not found
+	"""
+	first = 0
+	last = len(item_list)-1 
+	index = len(item_list)
+	while first < last:
+		mid = int((first + last) / 2)
+		if item_list[mid] >= item:
+			last = mid
 		else:
-			if item < item_list[mid]:
-				last = mid - 1 //shifting middle element's subset to the left
-			else:
-				first = mid + 1	//" " " " to the right
-	return found
+			first = mid + 1
+	if item_list[first] == item:
+		index = first
+	return index
 
-print(binary_search([1,2,3,5,8], 6))
-print(binary_search([1,2,3,5,8], 5))
+
+if __name__ == '__main__':
+	   ### Tests ###
+	print(binary_search([1,2,3,5,8], 6)) # returns len(item_list)
+	print(binary_search([1,2,3,5,8], 5)) # returns 3
+	print(binary_search([1, 2, 3, 3, 3, 4, 4, 5, 10], 4)) # returns 5
+
+
