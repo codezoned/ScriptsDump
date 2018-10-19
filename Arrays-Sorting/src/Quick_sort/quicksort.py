55b4935fa71cc94f721b4f755471bd38411a0f99
Gilles Igot <gilles.igot@gmail.com>
2018-10-19 20:52:19 +0200

    Add docstring and comments.

--- Arrays-Sorting/src/Quick_sort/quicksort.py
+++ Arrays-Sorting/src/Quick_sort/quicksort.py
@@ -1,4 +1,9 @@
 def quickSort(arr):
+    """Apply quick sort on the given array
+
+    :param arr: the array to sort
+    :type arr: list
+    """
     less = []
     pivotList = []
     more = []
@@ -17,6 +22,16 @@ def quickSort(arr):
         more = quickSort(more)
         return less + pivotList + more
 
-a = [4, 65, 2, -31, 0, 99, 83, 782, 1]
+# Unit test
+a = [4, 65, 2, -31, 0, 99, 83, 782, 1]  # The array to sort
 a = quickSort(a)
-assert all(a[i] <= a[i+1] for i in range(len(a)-1))
+assert all(a[i] <= a[i+1] for i in range(len(a)-1))  # Assert array is sorted
+
+# Quick sort: Quicksort is a comparison sort, meaning that it can 
+# sort items of any type for which a "less-than" relation is defined. 
+# In efficient implementations it is not a stable sort, meaning 
+# that the relative order of equal sort items is not preserved. 
+# Quicksort can operate in-place on an array, requiring small 
+# additional amounts of memory to perform the sorting. It is very 
+# similar to selection sort, except that it does not always choose 
+# worst-case partition.
