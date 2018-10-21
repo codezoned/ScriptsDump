893a90c5720f3f8a9d355d5445c3c324677fb517
Tono <antonio.nogueras@yahoo.com>
2018-10-21 16:56:34 -0500

    Update sort_by_dimension.py

--- Image_Processing/src/sort_by_dimension.py
+++ Image_Processing/src/sort_by_dimension.py
@@ -5,28 +5,26 @@ from math import ceil
 from time import sleep
 
 current_directory = listdir('.')
-files = []
-folders = []
+files = 0
+folders = 0
 
 def nameshorten(image):
     name_length = len(image)
     namediv_even = name_length / 2
     namediv_odd = int(ceil(name_length / 2))
 
-    if name_length % 2 == 0 and name_length > 50: #even
-        return "{} ... {} ".format(image[:namediv_even - namediv_even / 2],
-                                   image[- int(ceil(namediv_even / 2)):])
+    def range_maker(designated_var):
+      return f'{image[:designated_var - designated_var / 2]} ... {image[- int(ceil(designated_var/ 2)):]}'
 
+    if name_length % 2 == 0 and name_length > 50: #even
+      range_maker(namediv_even)
     elif name_length % 2 != 0 and name_length > 50: #odd
-        return "{} ... {} ".format(image[:namediv_odd - namediv_odd / 2],
-                                   image[- int(ceil(namediv_odd / 2)):])
+      range_maker(namediv_odd)
     else:
         return image
 
 def imagesize(x):
-    width = str(image.open(x).width)
-    height = str(image.open(x).height)
-    return "{}x{}".format(width, height)
+    return f"{str(image.open(x).width)}x{str(image.open(x).height)}"
 
 for i in current_directory:
     try:
@@ -34,9 +32,9 @@ for i in current_directory:
         imgsize = imagesize(i)
         if not path.exists(imgsize):
             mkdir(imgsize)
-            folders.append('.')
+            folders =+ 1
         else:
-            print "Image: {} -> Folder: '{}'".format(nameshorten(i), imgsize)
+            print(f"Image: {nameshorten(i)} -> Folder: '{imgsize}'")
     except IOError:
         continue
 
@@ -44,14 +42,15 @@ for i in current_directory:
     try:
         imgsize = imagesize(i)
         fileformat = image.open(i).format
-        if fileformat == 'JPEG' or fileformat == 'BMP' or fileformat == 'GIF' or fileformat == 'PNG':
+        if fileformat in ('JPEG', 'BMP', 'GIF', 'PNG'):
             mover(i, imgsize)
-            files.append('*')
+            print(f"Image: {nameshorten(i)} -> Folder: '{imgsize}'")
+            files =+ 1
     except IOError:
         continue
 
-print "------------------"
-print "  \n Sorting complete!\n"
-print "- Files sorted: {}".format(len(current_directory))
-print "- Images moved: {}".format(len(files))
-print "- Folders created: {}".format(len(folders))
+print ("------------------")
+print ("  \n Sorting complete!\n")
+print (f"- Files sorted: {len(current_directory)}")
+print (f"- Images moved: {files}")
+print (f"- Folders created: {folders}")
