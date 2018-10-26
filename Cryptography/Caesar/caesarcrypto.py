b0c3fe2ffe334d5aea7d61cd191c04f69b44a821
wsjan <44316905+wsjan@users.noreply.github.com>
2018-10-26 16:53:38 +0200

    Update caesarcrypto.py

--- Cryptography/Caesar/caesarcrypto.py
+++ Cryptography/Caesar/caesarcrypto.py
@@ -1,6 +1,6 @@
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
-            'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-
+import string
+alphabet=list(string.ascii_lowercase)
+alphalen=len(alphabet)
 
 def encrypt(input, key):
     plaintext = ''
@@ -8,11 +8,10 @@ def encrypt(input, key):
         if x is not ' ':
             plaintext += x
     ciphertext = ''
+            
     for x in plaintext:
         if x in alphabet:
-            step = alphabet.index(x)+key
-            if step > 25:
-                step -= 26
+            step = (alphabet.index(x)+key)%alphalen
             ciphertext += alphabet[step]
         else:
             ciphertext += x
@@ -22,12 +21,10 @@ def encrypt(input, key):
 def decrypt(cipher, key):
     plaintext = ''
     for x in cipher:
-        step = alphabet.index(x)-key
-        if step < 0:
-            step += 26
+        step = (alphabet.index(x)-key)%alphalen
         plaintext += alphabet[step]
     return plaintext
 
-text = encrypt('my name is chef', 3)
-text2 = decrypt(text, 3)
-print "%s \n%s" % (text, text2)
+text = encrypt('my name is chef', 150)
+text2 = decrypt(text, 150)
+print ("%s \n%s" % (text, text2))
