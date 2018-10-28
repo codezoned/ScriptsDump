e1b7d121551ef98f513415246436875fd95e2531
MuriloAndre2000 <40254653+MuriloAndre2000@users.noreply.github.com>
2018-10-28 18:46:06 -0300

    Update prime_numbers.py

--- Mathematical_Algorithms/src/prime_numbers.py
+++ Mathematical_Algorithms/src/prime_numbers.py
@@ -3,13 +3,13 @@
 print("Type a number")
 n = input()
 a = 2
-b = True
+b = True #A boolean that tells us, if it is true so is a prime number, if not, is not a prime number
 
 
-while a < int(n):
+while a < int(n):#This will divide n to every natural number between 1 and n, a prime number wouldn't get a integer number 
 	if int(n) % a == 0 :
 		print("n is not a prime number!")
-		a = int(n)
+		a = int(n)#End this while
 		b = False
 		
 	a = a + 1
