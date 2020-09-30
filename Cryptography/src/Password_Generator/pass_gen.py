3b3e0fcb7732843d158dc598d5a4fe5e55ce2537
Mohit Kumar <mkay6123@gmail.com>
2020-10-01 00:47:47 +0530

    Update pass_gen.py

--- Cryptography/src/Password_Generator/pass_gen.py
+++ Cryptography/src/Password_Generator/pass_gen.py
@@ -5,15 +5,18 @@ import random
 #main
 def GeneratePassword (passLength):
     password = string.ascii_letters + string.digits + "!@#$%^&*()_+=-./?><|\}{[]"
-    #generating characters, numbers and letters for password
+    ''' Generating characters, numbers and letters for password
+        Use the string constant string.ascii_letters to get all the  lower and upper case letters.
+    '''
     passwordList = []
     #password list is for putting all the selected password units into a string and show them to user
     for passChar in range(passLength):
-        passRandom = random.choice(password)
-        passwordList.append(passRandom)
+        passRandom = random.choice(password) #The random.choice() function is used to choose a single item from any sequence.
+        passwordList.append(passRandom) # appends the randomly picked character from password. 
 
-    finalOutput = "".join(passwordList)
+    finalOutput = "".join(passwordList) # if password length == user's desired poass length returns password. 
     return finalOutput
+
 import CreatePasswords as MyPassword
 
 while 1:
