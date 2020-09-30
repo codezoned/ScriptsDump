c64d6f46760d561b2f81f924970f71ed8e94e300
Dipto Chakrabarty <45638240+DiptoChakrabarty@users.noreply.github.com>
2020-10-01 00:30:07 +0530

    add commands to run and steps to use

--- Automation/src/multiple-ec2-instances/ec2.py
+++ Automation/src/multiple-ec2-instances/ec2.py
@@ -3,6 +3,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 #Load env variables
+# Add all these environment variables in your env file
+# format of env file has been provided by adding an empty .env
 access_key= os.getenv("ACCESS_KEY")
 secret_key= os.getenv("SECRET_KEY")
 ami= os.getenv("AMI")
@@ -19,10 +21,12 @@ ec2 = boto3.resource('ec2', region_name=region, aws_access_key_id= access_key,aw
 instance = ec2.create_instances(
     ImageId = ami,
     MinCount = 1,
-    MaxCount = {specify max instances needed here},
-    InstanceType = type,
-    KeyName = key_name,
+    MaxCount = {specify max instances needed here},  # here replace brancket with the number of instances you need 
+    InstanceType = type,   
+    KeyName = key_name,  
     SubnetId = subnet)
 
 instance.wait_until_running()
 print("Instance Up and Running")
+
+# To use the script run python3 ec2.py 
