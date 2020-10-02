b00c6d4eefd90e1657526ab8553da5657e2cef1e
Sheetal <pandeysheetal777@gmail.com>
2020-10-02 22:34:04 +0530

    Add files via upload

---
+++ Arrays-searching/src/linear_search.py
@@ -0,0 +1,25 @@
+def search(arr, n, x): 
+
+  	for i in range (0, n): 
+
+        if (arr[i] == x): 
+
+            return i; 
+
+    return -1; 
+
+arr = [ 2, 3, 4, 10, 40 ]; 
+
+x = 10; 
+
+n = len(arr); 
+
+result = search(arr, n, x) 
+
+if(result == -1): 
+
+    print("Element is not present in array") 
+
+else: 
+
+    print("Element is present at index", result);
