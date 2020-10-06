473cbb2264e99a1070a299c562e8a0328eaea624
Mohit Kumar <mkay6123@gmail.com>
2020-10-07 00:11:09 +0530

    Update topological_sort.py

--- Arrays-Sorting/src/Topological Sort/topological_sort.py
+++ Arrays-Sorting/src/Topological Sort/topological_sort.py
@@ -1,17 +1,30 @@
+''' 
+     TOPOLOGICAL SORT :
+     Topological sorting for Directed Acyclic Graph (DAG) is a linear ordering of vertices such that for every directed edge u v, 
+     vertex u comes before v in the ordering. Topological Sorting for a graph is not possible if the graph is not a DAG.
+     
+     For more Information about Topological Sort visit : https://cp-algorithms.com/graph/topological-sort.html
+
+     You are given a directed graph with n vertices and m edges. 
+     You have to number the vertices so that every edge leads from the vertex with a smaller number assigned to the vertex with a larger one.     
+'''
+
 from __future__ import print_function
+
 '''
      a
     / \
    b  c
   / \
   d  e
- '''
+'''
+
 edges = {'a': ['c', 'b'], 'b': ['d', 'e'], 'c': [], 'd': [], 'e': []}
 vertices = ['a', 'b', 'c', 'd', 'e']
 
 
 def topological_sort(start, visited, sort):
-    """Perform topolical sort on a directed acyclic graph."""
+   #Perform topolical sort on a directed acyclic graph.
     current = start
     # add current to visited
     visited.append(current)
@@ -32,4 +45,12 @@ def topological_sort(start, visited, sort):
 
 
 sort = topological_sort('a', [], [])
-print(sort)
+print(sort) # prints the sorted array
+
+''' 
+     OUTPUT : ['c', 'd', 'e', 'b', 'a']
+     
+     Time Complexity: O(V+E). 
+     The above algorithm is simply DFS so time complexity is the same as DFS which is. O(V+E).
+
+'''
