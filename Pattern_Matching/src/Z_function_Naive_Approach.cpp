23f9a1c6d5f70b563cbf0693ead74c4fb70e0572
nirmalkatiyar <87941922+nirmalkatiyar@users.noreply.github.com>
2021-10-09 21:47:21 +0530

    Update and rename Z_function_Strings_Algo.cpp to Z_function_Naive_Approach.cpp

--- Pattern_Matching/src/Z_function_Naive_Approach.cpp
+++ Pattern_Matching/src/Z_function_Naive_Approach.cpp
@@ -5,23 +5,31 @@ using namespace std;
 //O(n^2)
 vector<int>Z_function(string s){
     int n = s.size();
-    //store the values of z function
+    //vector z store the  z  values 
+    //i.e.(i-th element is equal to the greatest number of characters starting from the position i that coincide with the first characters of s)
     vector<int> z(n,0); 
     //Traversing the whole string 
     for (int i = 1; i < n; ++i)
-    //comparing the characters and incresing the range 
+    //comparing the characters and incresing the range i.e. length of substring which previously exists 
         while (i + z[i] < n && s[z[i]] == s[i + z[i]])
             ++z[i];
     return z;
 }
+
 int main(){
     string text;
     string pat;
     cin>>text>>pat;
+    //making a temporary string total
     string total=pat+"$"+text;
     vector<int>OutputIndices=Z_function(total);
+    //Z_function will return the array/vector of indices ie
+    //i-th element is equal to the greatest number of characters starting from the position i that coincide with the first characters of temporary string total.
+    //if size of pattern matches with any value of vector returned by z_function it means our pattern found in temporary string 
+    // but to  get real index of pattern in given text we have to substract  pattern length and 1 (for special character) from the index of vector returned by Z_function.
     for(auto i=0;i<OutputIndices.size();i++){
         if(OutputIndices[i]==pat.size()){
+            
             cout<<(i-pat.size()-1)<<" ";
         }
     }
