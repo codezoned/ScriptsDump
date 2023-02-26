86d3ee6897daff6c96bccacc98d9773e2f9a45e2
Hassenbd <hassenbendhieb203@gmail.com>
2023-02-26 22:27:38 +0100

    La fonction test_push crée une pile de taille 2, ajoute deux éléments à la pile et essaie d'ajouter un troisième élément à la pile pleine. On utilise pytest.raises pour vérifier si l'ajout d'un troisième élément à une pile pleine génère une exception IndexError. La fonction test_pop crée une pile de taille 2, essaie de supprimer un élément d'une pile vide, ajoute deux éléments à la pile, supprime les éléments de la pile et essaie de supprimer un élément d'une pile vide. On utilise pytest.raises pour vérifier si la suppression d'un élément d'une pile vide génère une exception IndexError. Les fonctions test_isEmpty et test_isFull créent une pile de taille 2, vérifient si la pile est vide/pleine avant et après avoir ajouté des éléments à la pile. added tests that cover the stack.py .closes #68

---
+++ Data_Structure/src/Stack/test_Stack.py
@@ -0,0 +1,38 @@
+
+def test_push():
+    # Créer une pile de taille 2
+    s = Stack(2)
+    s.push(1)
+    s.push(2)
+    # Essayer d'ajouter un élément à une pile pleine
+    with pytest.raises(IndexError):
+        s.push(3)
+
+def test_pop():
+    # Créer une pile de taille 2
+    s = Stack(2)
+    # Essayer de supprimer un élément d'une pile vide
+    with pytest.raises(IndexError):
+        s.pop()
+    s.push(1)
+    s.push(2)
+    assert s.pop() == 2
+    assert s.pop() == 1
+    # Essayer de supprimer un élément d'une pile vide
+    with pytest.raises(IndexError):
+        s.pop()
+
+def test_isEmpty():
+    # Créer une pile de taille 2
+    s = Stack(2)
+    assert s.isEmpty() == True
+    s.push(1)
+    assert s.isEmpty() == False
+
+def test_isFull():
+    # Créer une pile de taille 2
+    s = Stack(2)
+    assert s.isFull() == False
+    s.push(1)
+    s.push(2)
+    assert s.isFull() == True
